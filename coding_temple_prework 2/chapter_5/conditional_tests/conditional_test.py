f3b9189e41d7176f1359e5d755a80bed110b8007
andy kraus <andrew521@gmail.com>
2022-08-27 15:52:47 -0600

    this is the saved state

--- coding_temple_prework 2/chapter_5/conditional_tests/conditional_test.py
+++ coding_temple_prework 2/chapter_5/conditional_tests/conditional_test.py
@@ -1,3 +1,6 @@
+from os import remove
+
+
 my_string = "hello there!"
 my_string_2 = "hellothere"
 print(my_string == my_string_2)
@@ -6,12 +9,8 @@ print(my_string != my_string_2)
 print(my_string == my_string_2.lower())
 print(my_string <= my_string_2)
 
-
-
-
 print(my_string >= my_string_2)
 print(my_string <= my_string_2)
-
 
 print(my_string > my_string_2)
 print(my_string < my_string_2)
@@ -28,5 +27,10 @@ if listOfStrings.count("hit") > 0 :
 if listOfStrings.count("zoom") < 1:
     print("no, 'zoom' found in List : " , listOfStrings)
     
-
-    
+user_names = ["andy","admin","jack","ryan"]
+admin_name = "admin"
+for user in user_names:
+    if user == admin_name:
+      print("hello admin I hope your having a wonderful day!")
+    else:
+      print("hello ",user)
